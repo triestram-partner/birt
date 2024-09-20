2c4b064e5bc4d6ecba1a73d03f58e98c1ba5a974
Henning von Bargen <h.vonbargen@t-p.com>
2024-09-20 17:54:25 +0200

    Copying tagType in EngineIRVisitor seems superfluous and caused the EngineIRIOTest to fail.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/parser/EngineIRVisitor.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/parser/EngineIRVisitor.java
@@ -1673,10 +1673,6 @@ public class EngineIRVisitor extends DesignVisitor {
 		}
 
 		handleAltText(handle, item);
-
-		// Copy tagType
-		String tagType = handle.getTagType();
-		item.setTagType(tagType);
 	}
 
 	/**
